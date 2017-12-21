88306a9a642f1ea7f0253adaca28463ad243e288
Marty Spellerberg <marty@martyspellerberg.com>
2017-12-20 18:33:18 -0600

    Some setup

---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>Corrado Financial Group</title>
+
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon.ico">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-180.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-152.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-144.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-120.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-76.png" />
+	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-60.png" />
+	<meta name="application-name" content="<?php bloginfo( 'name' ); ?>"/> 
+	<meta name="msapplication-TileColor" content="#000000"/> 
+	<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-144.png"/>
+
+	<?php wp_head(); ?>
+
+</head>
+
+<body>
+
+<header>
+Corrado Financial Group
+
+Login
+Contact
+
+</header>
+
+<section class="intro">
+
+<h1>We help our clients plan and chart their financial futures.</h1>
+
+<img src="" />
+
+</section>
+
+<section class="info">
+
+<h2>Who We Are</h2>
+
+<p>We began as an accounting firm. In the two decades since, we&nbsp;ve come to understand that many of our clients needed more than someone to file their tax returns—they needed advisors who understand the full scope of their financial lives. They needed guidance on their investment portfolio, help with planning for retirement, direction on writing their wills, their pre-nups, their insurances.</p> 
+
+<p>We&nbsp;ve found that our relationships-first approach to financial planning gives our clients the freedom to realize their best selves. We help our clients plan for their financial future — so that they can live more fully in the present.</p>
+
+<h2>What We Do</h2>
+
+<ul>
+<li>Wealth Management</li>
+<li>Tax Planning</li>
+<li>Retirement &amp; Estate Planning</li>
+<li>Insurance</li>
+<li>Business Services</li>
+</ul>
+
+<h2>Contact Us</h2>
+
+<h3>General</h3>
+
+<address><a href="mailto:info@corradofg.com">info@corradofg.com<a/><br />
+201.661.6600</address>
+
+<h3>New Business</h3>
+
+<address>Daniel P. Rosaschi<br />
+<a href="mailto:drosaschi@corradofg.com">drosaschi@corradofg.com</a></address>
+
+<h3>New Jersey</h3>
+
+<address>48 S. Franklin Turnpike<br />
+Suite #300<br />
+Ramsey, NJ 07446</address>
+
+<h3>New York</h3>
+
+<address>25 W. 43rd Street<br />
+Suite #920<br />
+New York, NY 10036</address>
+
+</section>
+
+<?php wp_footer(); ?>
+
+</body>
+</head>
