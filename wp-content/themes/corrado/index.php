588b54e04eef5f8bd146918b8d95579eafd95380
Marty Spellerberg <marty@martyspellerberg.com>
2017-12-26 15:59:36 -0600

    Move git to root folder

---
+++ wp-content/themes/corrado/index.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>Corrado Financial Group</title>
+
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon.ico">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-180.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-152.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-144.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-120.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-76.png" />
+	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-60.png" />
+	<meta name="application-name" content="<?php bloginfo( 'name' ); ?>"/> 
+	<meta name="msapplication-TileColor" content="#000000"/> 
+	<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-144.png"/>
+
+	<?php wp_head(); ?>
+
+</head>
+
+<body>
+
+<header class="banner" role="banner">
+
+	<div class="logo">
+		<span class="screenreadertext">Corrado Financial Group</span>
+	</div>
+
+	<nav role="navigation">
+		<ul>
+			<li><a href="">Login</a></li>
+			<button>Contact</button>
+		</ul>
+	</nav>
+
+</header>
+
+<div class="intro">
+<div class="margin">
+<h1>We help our clients plan and chart their financial futures.</h1>
+
+<img src="<?php echo get_stylesheet_directory_uri() ?>/images/illustration.jpg" />
+</div>
+</div>
+
+<main role="main">
+
+<h2>Who We Are</h2>
+
+<p>We began as an accounting firm. In the two decades since, we&rsquo;ve come to understand that many of our clients needed more than someone to file their tax returns—they needed advisors who understand the full scope of their financial lives. They needed guidance on their investment portfolio, help with planning for retirement, direction on writing their wills, their pre-nups, their insurances.</p> 
+
+<p>We&rsquo;ve found that our relationships-first approach to financial planning gives our clients the freedom to realize their best selves. We help our clients plan for their financial future &mdash; so that they can live more fully in the present.</p>
+
+<h3>What We Do</h3>
+
+<ul>
+<li>Wealth Management</li>
+<li>Tax Planning</li>
+<li>Retirement &amp; Estate Planning</li>
+<li>Insurance</li>
+<li>Business Services</li>
+</ul>
+
+</main>
+
+<footer role="contentinfo">
+
+<h2>Contact Us</h2>
+
+<div class="items">
+
+	<div class="item">
+
+		<h3>General</h3>
+
+		<address><a href="mailto:info@corradofg.com">info@corradofg.com</a><br />
+		201.661.6600</address>
+
+	</div>
+
+	<div class="item">
+
+		<h3>New Business</h3>
+
+		<address>Daniel P. Rosaschi<br />
+		<a href="mailto:drosaschi@corradofg.com">drosaschi@corradofg.com</a></address>
+
+	</div>
+
+	<div class="item">
+
+		<h3>New Jersey</h3>
+
+		<address>48 S. Franklin Turnpike<br />
+		Suite #300<br />
+		Ramsey, NJ 07446</address>
+
+	</div>
+
+	<div class="item">
+
+		<h3>New York</h3>
+
+		<address>25 W. 43rd Street<br />
+		Suite #920<br />
+		New York, NY 10036</address>
+
+	</div>
+
+</div>
+
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</head>
