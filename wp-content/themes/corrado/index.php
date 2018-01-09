2e992185b07911425e68166c96119e15deb4779e
Marty Spellerberg <marty@martyspellerberg.com>
2018-01-09 15:16:15 -0600

    Favicons, scrolling

--- wp-content/themes/corrado/index.php
+++ wp-content/themes/corrado/index.php
@@ -9,14 +9,14 @@
 
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon.ico">
 	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-180.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-167.png" />
 	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-152.png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-144.png" />
 	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-120.png" />
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-76.png" />
-	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-60.png" />
+	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-120.png" />
+	<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon.svg" color="black">
 	<meta name="application-name" content="<?php bloginfo( 'name' ); ?>"/> 
 	<meta name="msapplication-TileColor" content="#000000"/> 
-	<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-144.png"/>
+	<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri() ?>/images/favicons/favicon-180.png"/>
 
 	<?php wp_head(); ?>
 
@@ -37,8 +37,8 @@
 
 			<nav role="navigation">
 				<ul>
-					<li><a href="http://corrado.local/">Login</a></li>
-					<li><button class="tocontact">Contact</button></li>
+					<li><a target="_blank" href="https://www.clientaxcess.com/#/login">Login</a></li>
+					<li><a href="#contactus" class="button tocontact">Contact</a></li>
 				</ul>
 			</nav>
 
