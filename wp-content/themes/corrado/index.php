5c704fe0f84b992dfb82b143f47c26d583c4f3dc
Marty Spellerberg <marty@martyspellerberg.com>
2018-08-20 17:21:25 -0500

    Default page styling #10

--- wp-content/themes/corrado/index.php
+++ wp-content/themes/corrado/index.php
@@ -2,13 +2,19 @@
 
 <?php if (have_posts()) : ?>
 
-	<?php while (have_posts()) : the_post(); ?>
+	<div class="pagetemplate">
+	<div class="pagepadding">
 
-		<h1><?php the_title(); ?></h1>
+		<?php while (have_posts()) : the_post(); ?>
 
-		<?php the_content(); ?>
+			<h1><?php the_title(); ?></h1>
 
-	<?php endwhile; ?>
+			<?php the_content(); ?>
+
+		<?php endwhile; ?>
+
+	</div>
+	</div>
 
 <?php endif; ?>
 
