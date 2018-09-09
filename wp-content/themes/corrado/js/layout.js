feea9c2b1930b687ced369597185275c807c88f8
Marty Spellerberg <marty@martyspellerberg.com>
2018-09-09 13:35:05 -0500

    Formatting in IE11 #29

--- wp-content/themes/corrado/js/layout.js
+++ wp-content/themes/corrado/js/layout.js
@@ -17,6 +17,30 @@
 	var modalScrollTop = 0;
 	var modalisopen = false;
 
+	function isInternetExplorer() {
+
+	  var rv = -1;
+	  if (navigator.appName == 'Microsoft Internet Explorer')
+	  {
+	    var ua = navigator.userAgent;
+	    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+	    if (re.exec(ua) != null)
+	      rv = parseFloat( RegExp.$1 );
+	  }
+	  else if (navigator.appName == 'Netscape')
+	  {
+	    var ua = navigator.userAgent;
+	    var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+	    if (re.exec(ua) != null)
+	      rv = parseFloat( RegExp.$1 );
+	  }
+
+		if ( rv != -1 ) {
+			return true;
+		}
+
+	}
+
 	function scrollDisable() {
 
 		if ( modalisopen == false ) {
@@ -328,6 +352,10 @@
 	}
 
 	$(document).ready( function() {
+
+		if ( isInternetExplorer() ) {
+			$('body').addClass('ie');
+		}
 
 		$( ".menubutton" ).click(function() {
 		  topnavOpen();
